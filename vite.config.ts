6af595a37dac094ea4a655aad340641cdcf17c96
MBarry01 <barrymohamadou98@gmaail.com>
2025-01-22 22:25:08 +0100

    Ajout de la configuration de base pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/memorel/', // Remplacez "memorel" par le nom de votre dépôt GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/memorel/', // Ajoute cette ligne avec le nom de ton dépôt
 });
